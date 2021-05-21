3ca964ff9b314494a8797d8b38b253684465d17b
Lucho <l.contreras534@pascualbravo.edu.co>
2021-05-20 22:01:05 -0500

    fix node start

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    "mongodb+srv://socialmediaUser:bKMw2Rofp8xx5zL2@cluster0.9fq6s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  SECRET_KEY: "185E36DA0AF2A008B081590B460B7D39719E7F4E",
+};
